8b01947a56217ed765e31cc7c146ec75a8f085ed
Cole Snyder <csnyder14@winona.edu>
2023-04-20 10:13:30 -0500

    init

---
+++ alan-watts-electronic-poster/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace alan_watts_electronic_poster
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
