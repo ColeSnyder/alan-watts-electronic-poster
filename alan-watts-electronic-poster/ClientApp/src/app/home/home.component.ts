ed643ee0ed1e1181f34d8cc64c95f996efd20bcc
Cole Snyder <csnyder14@winona.edu>
2023-04-20 10:21:19 -0500

    push after client app setup

--- alan-watts-electronic-poster/ClientApp/src/app/home/home.component.ts
+++ alan-watts-electronic-poster/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import quotes from '../../assets/quotes/quotes.json';
+
+interface Quote {
+  quote: string
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
+
 export class HomeComponent {
+  quotes: Quote[] = quotes;
+}
+
+var quoteCounter = 0;
+
+setInterval(changeQuote, 120000);
+
+function changeQuote() {
+  if (quoteCounter == quotes.length) {
+    quoteCounter = 0;
+  }
+  document.getElementById("quote")!.innerHTML = quotes[quoteCounter].quote;
+  quoteCounter++;
 }
